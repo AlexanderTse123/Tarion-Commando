bb3591c39a0c2900981b07ad9ecf7cf20e2af6bb
Alexander Tse <alexandertse888@yahoo.com>
2020-08-03 13:37:25 -0400

    Final Commit

---
+++ Assets/My Assets/Scripts/Mainbuttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Mainbuttons : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
